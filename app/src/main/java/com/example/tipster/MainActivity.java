0b2ff59d86ac786d63d17df1ef481d7d969970a6
Hendrick <sebastinesoacatp@gmail.com>
2021-02-19 00:40:39 +0100

    Implementing app functionality

--- app/src/main/java/com/example/tipster/MainActivity.java
+++ app/src/main/java/com/example/tipster/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.tipster;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
@@ -45,47 +47,117 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    private View.OnClickListener mOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (v.getId() == R.id.btnCalculate)
+                calculate();
+            else
+                reset();
+        }
+    };
+
+    private void reset() {
+        txtTipAmount.setText("");
+        txtTotalToPay.setText("");
+        txtTipPerPerson.setText("");
+        txtAmount.setText("");
+        txtPeople.setText("");
+        txtTipOther.setText("");
+        rdoGroupTips.clearCheck();
+        rdoGroupTips.check(R.id.radioFifteen);
+        // Set focus on the first field
+        txtAmount.requestFocus();
+    }
+
+    private void calculate() {
+        Double billAmount = Double.parseDouble(txtAmount.getText().toString());
+        Double totalPeople = Double.parseDouble(txtPeople.getText().toString());
+        Double percentage = null;
+        boolean isError = false;
+
+        if (billAmount < 1.0){
+            showErrorAlert("Enter a valid Total Amount.", txtAmount.getId());
+            isError = true;
+        }
+
+        if (totalPeople < 1.0){
+            showErrorAlert("Enter a valid value for No. of people.", txtPeople.getId());
+            isError = true;
+        }
+
+        if (radioCheckedId == -1)
+            radioCheckedId = rdoGroupTips.getCheckedRadioButtonId();
+        if (radioCheckedId == R.id.radioFifteen)
+            percentage = 15.00;
+        else if (radioCheckedId == R.id.radioTwenty)
+            percentage = 20.00;
+        else if (radioCheckedId == R.id.radioOther){
+            percentage = Double.parseDouble(txtTipOther.getText().toString());
+            if (percentage < 1.0){
+                showErrorAlert("Enter a valid Tip percentage", txtTipOther.getId());
+                isError = true;
+            }
+        }
+
+        if (!isError){
+            Double tipAmount = ((billAmount * percentage) /100);
+            Double totalToPay = billAmount + tipAmount;
+            Double perPersonPays = totalToPay / totalPeople;
+
+            txtTipAmount.setText(tipAmount.toString());
+            txtTotalToPay.setText(totalToPay.toString());
+            txtTipPerPerson.setText(perPersonPays.toString());
+        }
+    }
+
+    private void showErrorAlert(String message, int id) {
+        new AlertDialog.Builder(this).setTitle("Error")
+                .setMessage(message).setNeutralButton("Close",
+                (dialog, which) -> findViewById(id).requestFocus()).show();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         initialiseDisplayContent();
-        rdoGroupTips.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                // Enable/disable Other Tip Percentage field
-                if (checkedId == R.id.radioFifteen || checkedId == R.id.radioTwenty){
-                    txtTipOther.setEnabled(false);
-                    /**
-                     * Enable the Calculate button if total Amount and NO. of
-                     * People fields have valid values.
-                     */
-                    btnCalculate.setEnabled(txtAmount.getText().length()>0
-                    & txtPeople.getText().length()>0);
-                }else if (checkedId == R.id.radioOther){
-                    // Enable the Other Tip Percentage field
-                    txtTipOther.setEnabled(true);
-                    // Set the focus to this field
-                    txtTipOther.requestFocus();
-                    /*
-                    * Here we enable the calculate button if the Total Amount and No.
-                    * of People fields have valid values. Also ensure that user
-                    * has entered an Other Tip Percentage value before enabling
-                    * the Calculate button.
-                    */
-                    btnCalculate.setEnabled(txtAmount.getText().length()>0
-                    && txtPeople.getText().length()>0
-                    && txtTipOther.getText().length()>0);
-                }
-                // To determine the tip percentage choice made by user
-                radioCheckedId = checkedId;
+        rdoGroupTips.setOnCheckedChangeListener((group, checkedId) -> {
+            // Enable/disable Other Tip Percentage field
+            if (checkedId == R.id.radioFifteen || checkedId == R.id.radioTwenty){
+                txtTipOther.setEnabled(false);
+                /**
+                 * Enable the Calculate button if total Amount and NO. of
+                 * People fields have valid values.
+                 */
+                btnCalculate.setEnabled(txtAmount.getText().length()>0
+                & txtPeople.getText().length()>0);
+            }else if (checkedId == R.id.radioOther){
+                // Enable the Other Tip Percentage field
+                txtTipOther.setEnabled(true);
+                // Set the focus to this field
+                txtTipOther.requestFocus();
+                /*
+                * Here we enable the calculate button if the Total Amount and No.
+                * of People fields have valid values. Also ensure that user
+                * has entered an Other Tip Percentage value before enabling
+                * the Calculate button.
+                */
+                btnCalculate.setEnabled(txtAmount.getText().length()>0
+                && txtPeople.getText().length()>0
+                && txtTipOther.getText().length()>0);
             }
+            // To determine the tip percentage choice made by user
+            radioCheckedId = checkedId;
         });
 
         txtAmount.setOnKeyListener(mKeyListener);
         txtPeople.setOnKeyListener(mKeyListener);
         txtTipOther.setOnKeyListener(mKeyListener);
+
+        btnCalculate.setOnClickListener(mOnClickListener);
+        btnReset.setOnClickListener(mOnClickListener);
 
     }
 
